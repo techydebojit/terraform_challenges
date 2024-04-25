ba60c76cecd076af339ee99a39d1c479c7b6eb86
Debojit Sadhukhan <debojitsadhukhan1994@gmail.com>
2024-04-25 12:59:10 +0530

    updating the deprecated part

--- Challenge_3/ec2-elastic-ip.tf
+++ Challenge_3/ec2-elastic-ip.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "eip" {
-  vpc = true
+  domain = "vpc"
   instance = aws_instance.citadel.id
   provisioner "local-exec" {
     command = "echo ${self.public_dns} >> /root/citadel_public_dns.txt"
